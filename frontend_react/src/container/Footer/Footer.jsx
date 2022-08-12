2a481ce8b664ff38db2f19b318be62e62a71fef3
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-08-12 16:39:38 -0400

    first commit

---
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div>Footer page</div>
+  )
+}
+
+export default Footer
---
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+// import ReactTooltip from 'react-tooltip';
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
+
+import "./Skills.scss";
+
+const Skill = () => {
+  return (
+    <>
+    <h2 className="head-text">Skills & Experience</h2>
+    </>
+  )
+}
+
+export default Skill
