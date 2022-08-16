c90884a79b54c1fa0c93501df4481f9f38d17f80
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-08-15 21:01:01 -0400

    ink the social media

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { BsTwitter, BsInstagram, BsLinkedin } from "react-icons/bs";
-import { FaFacebook } from "react-icons/fa";
+import { FaFacebook, FaYoutube } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <BsTwitter />
+        <a href="https://twitter.com/EzeKakana">
+          <BsTwitter />
+        </a>
       </div>
-      <div>
+      {/* <div>
         <FaFacebook />
+      </div> */}
+      <div>
+        <a href="https://www.linkedin.com/in/ezechiel-itimbien-4841aa92/">
+          <BsLinkedin />
+        </a>
       </div>
       <div>
-        <BsInstagram />
-      </div>
-      <div>
-        <BsLinkedin />
+        <a href="https://www.youtube.com/channel/UC_-rI0d4c6AmGA_3p2-ls9w">
+          <FaYoutube />
+        </a>
       </div>
     </div>
   );
