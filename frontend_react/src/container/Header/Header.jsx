0f36c13faa5e2dbdfbb969a78df3c8805d3b0b43
Ezechiel ITIMBIEN KAKANA <papaezechiel@gmail.com>
2022-08-19 00:47:02 -0400

    assets modify

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -34,7 +34,7 @@ const Header = () => (
 
         <div className="tag-cmp app__flex">
           <p className="p-text">Full Stack Web Developer</p>
-          <p className="p-text">Freelancer</p>
+          {/* <p className="p-text">Freelancer</p> */}
         </div>
       </div>
     </motion.div>
